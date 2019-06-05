5ac6b53cce07d3be6ad56be94d1f21811dbc908e
hernandez2019 <51342331+hernandez2019@users.noreply.github.com>
2019-06-04 18:47:35 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proeycto_progra_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un  algoritmo que imprima los número del 0 al 100
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
